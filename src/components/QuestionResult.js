cd263015759e64a3b4b78d22e18c4c87c8da47e3
Fredy Arias <fredyariasm@gmail.com>
2022-02-10 22:06:29 -0500

    Connect result state

--- src/components/QuestionResult.js
+++ src/components/QuestionResult.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 import ProgressBar from "react-bootstrap/ProgressBar"
+import { formatResults } from "../utils/helpers"
 
 
 class QuestionResult extends Component {
@@ -8,39 +9,41 @@ class QuestionResult extends Component {
 
     render() {
 
-        const { optionOne, optionTwo } = this.props
-        const now = 60
+        const { optionOne,
+            optionTwo,
+            optionOneVotes,
+            optionTwoVotes,
+            totalVotes,
+            percentageOne,
+            percentageTwo,
+            myAnswer } = this.props.results
+
 
         return (
             <div>
                 <h4>Results:</h4>
+                <div className={myAnswer === 'one' ? "result-selected-container" : "result-container"} >
+                    {myAnswer === 'one' && <div className="your-vote">Your vote</div>}
 
-                {/* className={completed ? 'text-strike' : null} */}
-
-                <div className="result-selected-container">
-                    <div className="your-vote">Your vote</div>
                     <p>Would you rather {optionOne}?</p>
-                    <ProgressBar now={now} label={`${now}%`} />
-                    <p className="votes">1 out of 2 votes</p>
+                    <ProgressBar now={percentageOne} label={`${percentageOne}%`} />
+                    <p className="votes">{optionOneVotes} out of {totalVotes} votes</p>
                 </div>
-                <div className="result-container">
-                    <p>Would you rather {optionOne}?</p>
-                    <ProgressBar now={now} label={`${now}%`} />
-                    <p className="votes">1 out of 2 votes</p>
+                <div className={myAnswer === 'two' ? "result-selected-container" : "result-container"} >
+                    {myAnswer === 'two' && <div className="your-vote">Your vote</div>}
+                    <p>Would you rather {optionTwo}?</p>
+                    <ProgressBar now={percentageTwo} label={`${percentageTwo}%`} />
+                    <p className="votes">{optionTwoVotes} out of {totalVotes} votes</p>
                 </div>
-
             </div>
         )
     }
 }
 
-function mapStateToProps({ questions }, { id }) {
-
-    const question = questions[id]
+function mapStateToProps({ questions, authedUser }, { id }) {
 
     return {
-        optionOne: question.optionOne.text,
-        optionTwo: question.optionTwo.text
+        results: formatResults(questions, id, authedUser)
     }
 }
 
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12,3 +12,29 @@ export function formatQuestion(question, author) {
     }
 
 }
+
+
+export function formatResults(questions, id, authedUser){
+    
+    const question = questions[id]
+    const totalVotes = question.optionOne.votes.length + question.optionTwo.votes.length
+    const optionOneVotes = question.optionOne.votes.length
+    const optionTwoVotes = question.optionTwo.votes.length
+    const percentageOne = Math.round(optionOneVotes/totalVotes * 100)
+    const percentageTwo = Math.round(optionTwoVotes/totalVotes * 100)
+    const myAnswer = question.optionOne.votes.includes(authedUser) ? 'one':'two'
+
+    return {
+        optionOne: question.optionOne.text,
+        optionTwo: question.optionTwo.text,
+        optionOneVotes,
+        optionTwoVotes,
+        percentageOne,
+        percentageTwo,
+        totalVotes,
+        myAnswer,
+    }
+}
+
+
+
